f9afe53d96d7227a3be41c4369cd248e2f986744
Fabian <fasm2296@gmail.com>
2017-03-05 19:46:05 -0600

    Se añade metodo de graficación para el porcentaje de error

--- src/ErrorFramework.h
+++ src/ErrorFramework.h
@@ -8,6 +8,8 @@
 #ifndef SRC_ERRORFRAMEWORK_H_
 #define SRC_ERRORFRAMEWORK_H_
 #include <iostream>
+#include <stdlib.h>
+#include <vector>
 #include <chrono>
 #include "DataStructures/xArray.h"
 #include "DataAccess/PyFacade.h"
@@ -53,6 +55,62 @@ public:
 	}
 
 	template<class dataType>
+	void graphRelativeError(int pFunctionMode, double pH, int pCoeffQuantity, int pCenter,
+			  	  	  	  	dataType pInitRange, dataType pEndRange){
+		PyFacade* xx = PyFacade::getInstance();
+
+		int nInit = pCoeffQuantity - 5;
+
+		if(pFunctionMode == 0){
+			int iMax = pEndRange / pH;
+
+			xArray<dataType> xValues(iMax);
+			xArray<dataType> y1Values(iMax);
+			xArray<dataType> y2Values(iMax);
+			xArray<dataType> y3Values(iMax);
+			xArray<dataType> y4Values(iMax);
+			xArray<dataType> y5Values(iMax);
+
+			for(int n = nInit; n < pCoeffQuantity; n++ ){
+
+				anpi::cos_a<dataType> pFunction(pCenter,n);
+				anpi::cos<dataType> pRefFunction(pCenter,n);
+
+				double valueX = pInitRange;
+
+
+
+
+				for(int i = 0; i < iMax; i++){
+					dataType yValue = pFunction(valueX);
+					dataType yRefValue = pRefFunction(valueX);
+
+					dataType currentErrorPercent = getAverageError(yRefValue,yValue);
+
+
+					if( (n - nInit) == 0)
+						y1Values.setArray(i,currentErrorPercent);
+					if( (n - nInit) == 1)
+						y2Values.setArray(i,currentErrorPercent);
+					if( (n - nInit) == 2)
+						y3Values.setArray(i,currentErrorPercent);
+					if( (n - nInit) == 3)
+						y4Values.setArray(i,currentErrorPercent);
+					if( (n - nInit) == 4)
+						y5Values.setArray(i,currentErrorPercent);
+
+					xValues.setArray(i,valueX);
+					valueX+=pH;
+				}
+
+			}
+
+			xx->create2DPlotError("cos",xValues,y1Values,y2Values,y3Values,y4Values,y5Values);
+
+		}
+	}
+
+	template<class dataType>
 	void graphRealvsAprox(int pFunctionMode, double pH, int pCoeffQuantity, int pCenter,
 						  dataType pInitRange, dataType pEndRange){
 		PyFacade* xx = PyFacade::getInstance();
@@ -68,7 +126,7 @@ public:
 			xArray<dataType> yRefValues(iMax);
 
 
-			double valueX = 0.2;
+			double valueX = pInitRange;
 			for(int i = 0; i < iMax; i++){
 
 				dataType yValue = pFunction(valueX);
@@ -83,7 +141,41 @@ public:
 				valueX+=pH;
 
 			}
-			xx->create2DPlot(xValues,yValues,yRefValues);
+			xx->create2DPlot("cos",xValues,yValues,yRefValues);
+		}
+		else if(pFunctionMode == 1){
+			anpi::ln_a<dataType> pFunction(pCenter,pCoeffQuantity);
+			anpi::ln<dataType> pRefFunction(pCenter,pCoeffQuantity);
+
+			int iMax = pEndRange / pH;
+
+			xArray<dataType> yValues(iMax);
+			xArray<dataType> xValues(iMax);
+			xArray<dataType> yRefValues(iMax);
+
+
+			double valueX = pInitRange;
+			for(int i = 0; i < iMax; i++){
+
+				//dataType yValue = pFunction(valueX);
+				//dataType yRefValue = pRefFunction(valueX);
+
+				dataType yValue = std::log(valueX);
+				dataType yRefValue = std::log(valueX);
+
+				yValues.setArray(i,yValue);
+				yRefValues.setArray(i,yRefValue);
+				xValues.setArray(i,valueX);
+
+				cout << valueX << " | " << yValue << " | " << yRefValue << endl;
+
+				valueX+=pH;
+
+			}
+			xx->create2DPlot("ln",xValues,yValues,yRefValues);
+		}
+		else{
+			cout << "pFunctionMode debe ser 0 o 1" << endl;
 		}
 	}
 };
--- src/GUI/graphics.py
+++ src/GUI/graphics.py
@@ -16,17 +16,53 @@ vPORT = 9090
 #############################################################################
 ################################# Graphics ##################################
 #############################################################################
-def createGraphic(x, y, xRef,yRef):
+def createGraphic(pLabel,x, y, xRef,yRef):
     fig, ax1 = plt.subplots()
     ax1.plot(x,y,'b-')
     ax1.set_xlabel('time (s)')  
-    ax1.set_ylabel('cos', color='b')
+    ax1.set_ylabel(pLabel, color='b')
     ax1.tick_params('y', colors='b')
     
     ax2 = ax1.twinx()
     ax2.plot(xRef, yRef, 'r-')
-    ax2.set_ylabel('cos Ref', color='r')
+    ax2.set_ylabel(pLabel + ' Ref', color='r')
     ax2.tick_params('y', colors='r')
+    
+    
+    fig.tight_layout()
+    plt.show()
+
+def createGraphicError(pLabel,x, y1,y2,y3,y4,y5):
+    fig, ax1 = plt.subplots()
+    ax1.plot(x,y1,'b-')
+    ax1.set_xlabel('time (s)')  
+    ax1.set_ylabel(pLabel, color='b')
+    ax1.tick_params('y', colors='b')
+    
+    ax2 = ax1.twinx()
+    ax2.plot(x, y1, 'r-')
+    ax2.set_ylabel(pLabel + ' Ref', color='r')
+    ax2.tick_params('y', colors='r')
+
+    ax3 = ax1.twinx()
+    ax3.plot(x, y2, 'r-')
+    ax3.set_ylabel(pLabel + ' Ref', color='r')
+    ax3.tick_params('y', colors='r')
+
+    ax4 = ax1.twinx()
+    ax4.plot(x, y3, 'r-')
+    ax4.set_ylabel(pLabel + ' Ref', color='r')
+    ax4.tick_params('y', colors='r')
+
+    ax5 = ax1.twinx()
+    ax5.plot(x, y4, 'r-')
+    ax5.set_ylabel(pLabel + ' Ref', color='r')
+    ax5.tick_params('y', colors='r')
+
+    ax6 = ax1.twinx()
+    ax6.plot(x, y5, 'r-')
+    ax6.set_ylabel(pLabel + ' Ref', color='r')
+    ax6.tick_params('y', colors='r')
     
     
     fig.tight_layout()
@@ -64,12 +100,20 @@ def listen():
             if(data != ""):
                 print(data)
                 infoR = data.split("#")
-
-                pX = ast.literal_eval(infoR[1])
-                pY = ast.literal_eval(infoR[2])
-                pYRef = ast.literal_eval(infoR[3])
-                createGraphic(pX,pY,pX,pYRef);
-                #manageData(data)
+                if(infoR[0] == "RvsA"):
+                    pX = ast.literal_eval(infoR[2])
+                    pY = ast.literal_eval(infoR[3])
+                    pYRef = ast.literal_eval(infoR[4])
+                    createGraphic(infoR[1],pX,pY,pX,pYRef);
+                elif(infoR[0] == "EvsH"):
+                    pX = ast.literal_eval(infoR[2])
+                    pY1 = ast.literal_eval(infoR[3])
+                    pY2 = ast.literal_eval(infoR[3])
+                    pY3 = ast.literal_eval(infoR[3])
+                    pY4 = ast.literal_eval(infoR[3])
+                    pY5 = ast.literal_eval(infoR[3])
+                    
+                    createGraphicError(infoR[1],pX,pY1,pY2,pY3,pY4,pY5);
             #time.sleep()
     server.close()
 
